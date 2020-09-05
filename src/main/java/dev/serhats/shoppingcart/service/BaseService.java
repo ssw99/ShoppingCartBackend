c700337f767bfdcd618fb8a7a828f2a5039eab13
Serhat <ss99@tuta.io>
2020-09-06 00:51:12 +0300

    getAll method added to BaseService interface

--- src/main/java/dev/serhats/shoppingcart/service/BaseService.java
+++ src/main/java/dev/serhats/shoppingcart/service/BaseService.java
@@ -5,10 +5,16 @@ import dev.serhats.shoppingcart.model.BaseModel;
 import dev.serhats.shoppingcart.model.repo.BaseRepo;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BaseService<Entity extends BaseModel, Repo extends BaseRepo<Entity>> {
     Repo getRepo();
+
+    @Transactional(readOnly = true)
+    default List<Entity> getAll() {
+        return getRepo().findAll();
+    }
 
     @Transactional(readOnly = true)
     default Optional<Entity> findById(long entityId) {
