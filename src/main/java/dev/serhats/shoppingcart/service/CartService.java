59549090d0880a7473ed6477090df15e54bc7061
Serhat <ss99@tuta.io>
2020-09-05 14:19:51 +0300

    CartService signatures are changed to throw EntityNotFoundException

--- src/main/java/dev/serhats/shoppingcart/service/CartService.java
+++ src/main/java/dev/serhats/shoppingcart/service/CartService.java
@@ -1,5 +1,6 @@
 package dev.serhats.shoppingcart.service;
 
+import dev.serhats.shoppingcart.exception.EntityNotFoundException;
 import dev.serhats.shoppingcart.model.Cart;
 import dev.serhats.shoppingcart.model.Product;
 import dev.serhats.shoppingcart.model.repo.CartRepo;
@@ -7,12 +8,12 @@ import dev.serhats.shoppingcart.model.repo.CartRepo;
 import java.util.List;
 
 public interface CartService extends BaseService<Cart, CartRepo> {
-    List<Product> getProducts(long cartId);
-    void addToCart(long cartId, Product product);
-    void addToCart(long cartId, long productId);
-    void removeOneFromCart(long cartId, Product product);
-    void removeOneFromCart(long cartId,long productId);
-    void removeAllFromCart(long cartId,Product product);
-    void removeAllFromCart(long cartId,long productId);
-    void clearCart(long cartId);
+    List<Product> getProducts(long cartId) throws EntityNotFoundException;
+    void addToCart(long cartId, Product product) throws EntityNotFoundException;
+    void addToCart(long cartId, long productId) throws EntityNotFoundException;
+    void removeOneFromCart(long cartId, Product product) throws EntityNotFoundException;
+    void removeOneFromCart(long cartId,long productId) throws EntityNotFoundException;
+    void removeAllFromCart(long cartId,Product product) throws EntityNotFoundException;
+    void removeAllFromCart(long cartId,long productId) throws EntityNotFoundException;
+    void clearCart(long cartId) throws EntityNotFoundException;
 }
