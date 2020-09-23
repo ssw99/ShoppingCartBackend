6bd27bff56a677dff4cdf31fc16c8008f2cc3371
Serhat <ss99@tuta.io>
2020-09-23 14:38:24 +0300

    Controllers refactored

--- src/main/java/dev/serhats/shoppingcart/api/v1/controller/CategoryController.java
+++ src/main/java/dev/serhats/shoppingcart/api/v1/controller/CategoryController.java
@@ -5,12 +5,11 @@ import dev.serhats.shoppingcart.api.v1.dto.CategoryResponse;
 import dev.serhats.shoppingcart.model.Category;
 import dev.serhats.shoppingcart.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,29 +19,29 @@ import java.util.stream.Collectors;
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @GetMapping
-    public List<CategoryResponse> getAll() {
-        return categoryService.getAll().stream().map(CategoryResponse::new).collect(Collectors.toList());
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public CategoryResponse create(@RequestBody CategoryRequest categoryRequest,
-                                   HttpServletRequest request,
-                                   HttpServletResponse response) {
-        Category category = new Category(categoryRequest.getName());
-        category = categoryService.save(category);
-        response.setHeader("Location", request.getRequestURL() + "" + category.getId());
-        return new CategoryResponse(category);
-    }
 
     @GetMapping("{id}")
     public CategoryResponse get(@PathVariable long id) {
         return new CategoryResponse(categoryService.getById(id));
     }
 
+    @GetMapping
+    public List<CategoryResponse> getAll() {
+        return categoryService.getAll().stream().map(CategoryResponse::new).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody CategoryRequest categoryRequest,
+                                    HttpServletRequest request) {
+        Category category = new Category(categoryRequest.getName());
+        category = categoryService.save(category);
+
+        URI location = URI.create(request.getRequestURL() + "/" + category.getId());
+        return ResponseEntity.created(location).build();
+    }
+
     @DeleteMapping("{id}")
-    public ResponseEntity<CategoryResponse> delete(@PathVariable long id) {
+    public ResponseEntity<?> delete(@PathVariable long id) {
         categoryService.delete(id);
         return ResponseEntity.ok().build();
     }
--- src/main/java/dev/serhats/shoppingcart/api/v1/controller/ProductController.java
+++ src/main/java/dev/serhats/shoppingcart/api/v1/controller/ProductController.java
@@ -7,12 +7,11 @@ import dev.serhats.shoppingcart.model.Product;
 import dev.serhats.shoppingcart.service.CategoryService;
 import dev.serhats.shoppingcart.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,33 +22,33 @@ public class ProductController {
     private final ProductService productService;
     private final CategoryService categoryService;
 
-    @GetMapping
-    public List<ProductResponse> getAll() {
-        return productService.getAll().stream().map(ProductResponse::new).collect(Collectors.toList());
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponse create(@RequestBody ProductRequest productRequest,
-                                  HttpServletRequest request,
-                                  HttpServletResponse response) {
-        Category category = categoryService.getById(productRequest.getCategoryId());
-        Product product = productService.save(new Product(
-                productRequest.getName(),
-                productRequest.getPrice(),
-                productRequest.getAmount(),
-                category));
-        response.setHeader("Location", request.getRequestURL() + "" + product.getId());
-        return new ProductResponse(product);
-    }
 
     @GetMapping("{id}")
     public ProductResponse get(@PathVariable long id) {
         return new ProductResponse(productService.getById(id));
     }
 
+    @GetMapping
+    public List<ProductResponse> getAll() {
+        return productService.getAll().stream().map(ProductResponse::new).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody ProductRequest productRequest,
+                                    HttpServletRequest request) {
+        Category category = categoryService.getById(productRequest.getCategoryId());
+        Product product = productService.save(new Product(
+                productRequest.getName(),
+                productRequest.getPrice(),
+                productRequest.getAmount(),
+                category));
+
+        URI location = URI.create(request.getRequestURL() + "/" + product.getId());
+        return ResponseEntity.created(location).build();
+    }
+
     @DeleteMapping("{id}")
-    public ResponseEntity<ProductResponse> delete(@PathVariable long id) {
+    public ResponseEntity<?> delete(@PathVariable long id) {
         productService.delete(id);
         return ResponseEntity.ok().build();
     }
